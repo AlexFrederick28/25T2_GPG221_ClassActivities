054eb032e03119e02484fc1cd3d57b2488f8def8
AlexFrederick28 <alexanderfrederick12@hotmail.com>
2025-06-24 13:40:50 +1000

    Added:  - UI for game window inc. descriptions and spawning  - Pathfinders can now look for random targets, and acquire new targets upon reaching the distination

--- Assets/Scripts/AiMovement/PathFinder.cs
+++ Assets/Scripts/AiMovement/PathFinder.cs
@@ -1,45 +1,59 @@
-using System;
+
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PathFinder : MonoBehaviour
 {
+    [SerializeField] private List<Transform> targetTransformList;
     [SerializeField] private Transform targetTransform;
     [SerializeField] private NavMeshPath path;
     
     [SerializeField] private int cornerIndex; // This keeps track of the next corner you want to go to
     [SerializeField] private float magnitudeToCorner;
+    [SerializeField] private float magnitudeTarget;
     public Vector3 currentPath;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     void FixedUpdate()
     {
         path = new NavMeshPath();
 
         if (NavMesh.CalculatePath(transform.position, targetTransform.position, int.MaxValue, path))
         {
-
+            CalculatePath();
+        }
+        if (magnitudeTarget < 1.5f)
+        {
+            ChooseTarget();
+        }
+        if (targetTransform == null)
+        {
+            ChooseTarget();
         }
 
-        //distanceToCorner = path.corners[cornerIndex].magnitude;
+    }
 
-        // See if we are close to the corner
+    private void ChooseTarget()
+    {
+        int randNumb = Random.Range(0, targetTransformList.Count);
+        targetTransform = targetTransformList[randNumb];
+    }
+
+    private void CalculatePath()
+    {
         if (path != null)
         {
             currentPath = path.corners[cornerIndex]; // keeps track of the current selected path corner vector
-            
+
             magnitudeToCorner = Vector3.Distance(transform.position, currentPath); // shows the distance to the next corner
-            
+
             if (cornerIndex > path.corners.Length)
             {
                 cornerIndex = 0;
             }
+
+            magnitudeTarget = Vector3.Distance(transform.position, targetTransform.position);
         }
-        
     }
 
     private void OnDrawGizmos()
